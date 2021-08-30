33420d680d3e43794c4e7a2743c6223c31f87b37
swingpenguin <swingpenguinn@gmail.com>
2021-08-30 04:29:11 -0700

    test -v1

--- js/app.js
+++ js/app.js
@@ -4,6 +4,11 @@
 
 // }
 
+
+var navUl = document.getElementById("nav-ul");
+var toggleIcon = document.getElementById("toggle-icon")
+
+
 var marker = new AMap.Marker({
     icon: "https://webapi.amap.com/theme/v1.3/markers/n/mark_b.png",
     position: [112.560913, 37.78817]
@@ -21,3 +26,19 @@ var map = new AMap.Map('map', {
 
 map.add(marker);
 map.add(roadNetLayer);
+
+
+
+function toggleNavList() {
+    if (navUl.className === "") {
+        navUl.className = "show-nav-list"
+    } else {
+        navUl.className = ""
+    }
+}
+
+document.addEventListener("click", function(e) {
+    if (e.target !== toggleIcon) {
+        navUl.className = ""
+    }
+})
